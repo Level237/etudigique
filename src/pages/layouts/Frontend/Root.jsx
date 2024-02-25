5de7c144a4d035ca7e9542477b46ece2cb763a4d
level237 <Bramslevel129@gmail.com>
2024-02-25 08:07:32 +0100

    homepage etudigique

--- src/pages/layouts/Frontend/Root.jsx
+++ src/pages/layouts/Frontend/Root.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../../components/Footer";
 import Header from "../../../components/Header"
 import {Outlet} from "react-router-dom"
 const RootLayout=()=>{
@@ -6,6 +7,7 @@ const RootLayout=()=>{
         <>
         <Header/>
             <Outlet/>
+        <Footer/>
         </>
         
     )
