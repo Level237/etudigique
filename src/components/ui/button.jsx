09a6daaf8562a380cbd3bf90bd14e1fbcc8935bb
level237 <Bramslevel129@gmail.com>
2024-02-26 11:34:01 +0100

    update

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,6 +10,9 @@ const Button=(props)=>{
   }else if(props.type==="secondary"){
     bgBtn="#cc3333"
     colorBtn="white"
+  }else if(props.type==="default"){
+    bgBtn="#000"
+    colorBtn="white"
   }
     let classeBtn=`inline-flex items-center bg-[${bgBtn}] text-${colorBtn} justify-center px-5 py-3 mr-3 text-base font-medium text-center   rounded-lg   focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 `
     return (
