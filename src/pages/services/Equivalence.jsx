6ad4e69b6ea449dd2f5374f40589bb610eb21558
level237 <Bramslevel129@gmail.com>
2024-03-03 17:33:03 +0100

    update bloc 2

--- src/pages/services/Equivalence.jsx
+++ src/pages/services/Equivalence.jsx
@@ -57,17 +57,69 @@ const Equivalence=(props)=>{
       <Button type="secondary" title="Passez un entretien"/>
     </div>
         </section>
-        <section className="flex  px-[100px] justify-center bg-[#cc3333] p-[20px]" >
-        
-          <div style={{ width:"45vh" }} className="mb-[-20px]">
+        <section className=" px-[100px] py-[48px] justify-center bg-[#cc3333] p-[20px]" >
+        <div className="flex">
+        <div style={{ width:"50vh" }} className="mb-[-60px]">
           <img className="w-[100%] mt-8 rounded-s-3xl rounded-e-3xl" src={about} alt=""/>
           </div>
           
-          <div style={{ flex:1 }}>
+          <div className="flex flex-col" style={{ flex:1 }}>
           <h2 class="my-8 text-2xl font-bold  text-white md:text-4xl">
           Notre expertise en matière d'équivalence de diplôme
       </h2>
+      <div class="divide-y grid grid-cols-2 space-y-4 divide-gray-100 dark:divide-gray-800">
+            <div class="mt-8 flex gap-4 md:items-center">
+              <div class="w-12 h-12 flex gap-4 rounded-full bg-[#ffcc3321]">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-[#ffcc33]">
+                  <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z" clip-rule="evenodd"></path>
+                </svg>        
+              </div>
+              <div class="w-5/6">
+                <h3 class="font-semibold text-lg  text-[#ffcc33]">Évaluation gratuite de votre situation et de vos besoins.</h3>
+                <p class="text-white">Asperiores nemo possimus nesciunt quam mollitia.</p>
+              </div> 
+            </div> 
+            <div class="pt-4 flex gap-4 md:items-center">
+              <div class="w-12 h-12 flex gap-4 rounded-full bg-[#ffcc3321]">  
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-[#ffcc33]">
+                  <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
+                </svg>                                      
+              </div>
+              <div class="w-5/6">
+                <h3 class="font-semibold text-lg text-[#ffcc33]">Constitution et vérification de votre dossier de demande.</h3>
+                <p class="text-white">Asperiores nemo possimus nesciunt quam mollitia.</p>
+              </div> 
+            </div> 
+            <div class="pt-4 flex gap-4 md:items-center">
+              <div class="w-12 h-12 flex gap-4 rounded-full bg-[#ffcc3321]">  
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-[#ffcc33]">
+                  <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
+                </svg>                                      
+              </div>
+              <div class="w-5/6">
+                <h3 class="font-semibold text-lg text-[#ffcc33]">Conseils personnalisés pour maximiser vos chances de succès.</h3>
+                <p class="text-white">Asperiores nemo possimus nesciunt quam mollitia.</p>
+              </div> 
+            </div> 
+            <div class="pt-4 flex gap-4 md:items-center">
+              <div class="w-12 h-12 flex gap-4 rounded-full bg-[#ffcc3321]">  
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 m-auto text-[#ffcc33]">
+                  <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
+                </svg>                                      
+              </div>
+              <div class="w-5/6">
+                <h3 class="font-semibold text-lg text-[#ffcc33]">Mise en relation avec les autorités compétentes.</h3>
+                <p class="text-white">Asperiores nemo possimus nesciunt quam mollitia.</p>
+              </div> 
+            </div>
           </div>
+          </div>
+        </div>
+          
+          
+        </section>
+        <section>
+          
         </section>
         </section>
         
