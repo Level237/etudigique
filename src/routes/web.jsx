6b9c18ebe16c72793e857b3ffc7286c5db63ad39
level237 <Bramslevel129@gmail.com>
2024-03-03 20:57:24 +0100

    page admission:init

--- src/routes/web.jsx
+++ src/routes/web.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom"
 import RootLayout from "../pages/layouts/Frontend/Root"
 import Homepage from "../pages/Homepage"
 import Equivalence from "../pages/services/Equivalence"
+import Admission from "../pages/services/Admission"
 
 export const routes=createBrowserRouter([
     {
@@ -14,6 +15,9 @@ export const routes=createBrowserRouter([
             },{
                 path:'equivalence-de-diplome',
                 element:<Equivalence title="Equivalence de Diplome"/>
+            },{
+                path:'admission',
+                element:<Admission title="Admission"/>
             }
         ]
     }
