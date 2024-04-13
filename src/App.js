eb1f55cc4e8308334eb618d5f20a044e8ce20c85
level237 <Bramslevel129@gmail.com>
2024-04-13 08:27:09 +0100

    typescript implementation:vite

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import {routes} from "./routes/web"
-function App() {
-  return <RouterProvider router={routes}/>
-}
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
-    
   },
   plugins: [],
 }
