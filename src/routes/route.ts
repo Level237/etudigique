996862dd3589a94461c95f6688a87df5ffc9b30c
level237 <Bramslevel129@gmail.com>
2024-04-13 09:14:39 +0100

    Footer structuration

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from '../pages/layouts/Root';
+import Homepage from '../pages/Homepage';
 
 
 export const routes=createBrowserRouter([
     {
         path:'/',
-        element:<Suspense><Root/></Suspense>,
+        element:<RootLayout/>,
+        children:[
+            {
+                path:'',
+                element:<Homepage/>
+            }
+        ]
        
     }
 ])
+
+export default routes;
