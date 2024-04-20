d8008414f7f6504d17bd4f781f2ef67ac9a8923a
level237 <Bramslevel129@gmail.com>
2024-04-20 09:50:52 +0100

    emailJs init

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -22,15 +22,19 @@ const  Contact=()=> {
     <div className="grid gap-6 mb-6 md:grid-cols-2">
         <div>
             <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 ">Nom</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 max-sm:p-5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+            <input type="text" name='first_name' id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 max-sm:p-5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your name" required />
         </div>
         <div>
             <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 text-black">Prenom</label>
-            <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+            <input type="text" name='last_name' id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Last name" required />
+        </div>
+        <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 text-black">Email</label>
+            <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" />
         </div>
         <div>
             <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-90">Pays</label>
-            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select name='country' id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option selected>Choisir un pays</option>
     <option value="Cameroun">Cameroun</option>
     <option value="Senegal">Senegal</option>
@@ -38,12 +42,14 @@ const  Contact=()=> {
     <option value="Congo">Congo</option>
     <option value="Gabon">Gabon</option>
     <option value="Guinée">Guinée</option>
+    <option value="Europe">Europe</option>
+    <option value="Amerique">Amerique</option>
     <option value="Autres">Autres</option>
   </select>
         </div>
         <div>
             <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-90">Services</label>
-            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select name='services' id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option selected>Choisir un service</option>
     <option value="Equivalence de diplome">Equivalence de diplome</option>
     <option value="Service d'admission">Service d'Admission</option>
@@ -53,11 +59,11 @@ const  Contact=()=> {
         </div>
         <div>
             <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-90">Numéro de Téléphone</label>
-            <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" required />
+            <input type="tel" name='phone' placeholder='enter your phone' id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"  required />
         </div>
-         <div>
+         <div className='flex-1'>
          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 ">Votre message</label>
-<textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400"></textarea>
+<textarea id="message" name='message' placeholder='enter your message' required rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:placeholder-gray-400"></textarea>
          </div>
 
 
